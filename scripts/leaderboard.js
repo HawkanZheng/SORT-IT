3a52324fdf97c2f6177dee9bdb37dd090e0d02b5
dkeber <44458520+dkeber@users.noreply.github.com>
2020-04-06 17:44:45 -0700

    firebase access to leaderboard.

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -9,8 +9,8 @@ let config = {
     storageBucket: "comp-1800-94b18.appspot.com",
     messagingSenderId: "254451731238",
     appId: "1:254451731238:web:ff1e74fe12719d02740fe7"
-  };
-  // Initialize Firebase
+};
+// Initialize Firebase
 firebase.initializeApp(config);
 
 // Get a reference to the database server.
@@ -49,14 +49,14 @@ function playGame() {
 const TOP_PLAYERS = 10;
 
 // Create a leaderboards reference.
-let leaders = db.collection('Users');
+let leaders = db.collection('Leaderboard');
 
 // Get leaderboard element.
 let boards = document.getElementById('leaderboards');
 
 // Add leaderboard information to the page.
 function leaderboard() {
-    
+
     // Organize users based on their score.
     let topTen = leaders.orderBy('Score', 'desc').limit(TOP_PLAYERS);
 
@@ -68,12 +68,13 @@ function leaderboard() {
             let players = doc.data();
 
             // Create a cell element.
-            boards.innerHTML += '<tr>'
-            + '<td>' + place + '<td>'
-            + '<td' + players.Name + '<td>'
-            + '<td>' + players.Score + '<td>'
-            + '<tr>';
-            
+            boards.innerHTML += '<tr>' +
+                '<td>' + place + '<td>' +
+                players.Name + '<td>' +
+                players.Score + '<td>' +
+                players.School + '<td>'+
+                '<tr>';
+
             // increment the users rank.
             place++;
         })
@@ -82,3 +83,65 @@ function leaderboard() {
 
 // Call the function.
 leaderboard();
+
+
+//------------------------------------------------------
+// Add Game
+//------------------------------------------------------ 
+
+function addGame(outcome) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // Get the currently signed in users UID
+            let id = user.uid;
+
+            // Create reference for database.
+            let db = firebase.firestore();
+
+            // Create reference for Users collection.
+            let ref = db.collection('Users');
+
+            // Get user data.
+            ref.doc(id).get().then(function (doc) {
+
+                let highScore = doc.data().HighScore; // Assign the users current wins
+                let gamesPlayed = doc.data().GamesPlayed; // Grab number of games played.
+
+                gamesPlayed++; // increment games played.
+
+                // Create a time stamp for the game.
+                let date = new Date();
+                let timestamp = date.getTime();
+
+                // Check if the game score is greater than the users current highscore.
+                if (outcome > highScore) {
+                    highScore = outcome; // Increment wins
+                } else {
+                    highScore = prevScore; // Increment losses
+                }
+
+                // Update the users wins, loses, and last time played.
+                ref.doc(id).update({
+                    'LastTimePlayed': timestamp,
+                    'GamesPlayed': gamesPlayed,
+                    'HighScore': highScore
+                }).then(function () {
+                    // Send to landing page.
+                    location.replace('homePage.html');
+
+                    // log an error in the console.
+                }).catch(function (error) {
+                    console.error('Error creating game: ', error);
+                    window.alert('Error Game failed to uplaod to server.');
+
+                    // Send to landing page.
+                    location.replace('homePage.html');
+                });
+            })
+        } else {
+            // If no user is signed in.
+            console.log('no user');
+        }
+    })
+}
--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,17 @@
 //--------------------------------------------------------------
 // Your web app's Firebase configuration
 //--------------------------------------------------------------
-  let config = {
-    apiKey: "AIzaSyCI3tPf61sTY3nLoHJG7P8TGBakZU69o3w",
-    authDomain: "comp-1800-94b18.firebaseapp.com",
-    databaseURL: "https://comp-1800-94b18.firebaseio.com",
-    projectId: "comp-1800-94b18",
-    storageBucket: "comp-1800-94b18.appspot.com",
-    messagingSenderId: "254451731238",
-    appId: "1:254451731238:web:ff1e74fe12719d02740fe7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
+let config = {
+  apiKey: "AIzaSyCI3tPf61sTY3nLoHJG7P8TGBakZU69o3w",
+  authDomain: "comp-1800-94b18.firebaseapp.com",
+  databaseURL: "https://comp-1800-94b18.firebaseio.com",
+  projectId: "comp-1800-94b18",
+  storageBucket: "comp-1800-94b18.appspot.com",
+  messagingSenderId: "254451731238",
+  appId: "1:254451731238:web:ff1e74fe12719d02740fe7"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
 
 // Get a reference to the database server.
 let db = firebase.firestore();
@@ -22,27 +22,27 @@ let auth = firebase.auth();
 //------------------------------------------------------ 
 function createUser() {
 
-    let theEmail = document.getElementById('username');
-    let pass = document.getElementById('password');
+  let theEmail = document.getElementById('username');
+  let pass = document.getElementById('password');
 
-    let email = theEmail.value;
-    let password = pass.value;
+  let email = theEmail.value;
+  let password = pass.value;
 
-    auth.createUserWithEmailAndPassword(email, password).then(function () {
+  auth.createUserWithEmailAndPassword(email, password).then(function () {
 
-        // Send them to landing page
-        location.replace('studyGuide.html');
+    // Send them to landing page
+    location.replace('studyGuide.html');
 
-    }).catch(function (error) {
-        // Handle Errors here.
-        if (password.length < 6) {
-            window.alert('Password needs to be at least 6 characters.');
-        }
-        window.alert('That is Invalid');
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
+  }).catch(function (error) {
+    // Handle Errors here.
+    if (password.length < 6) {
+      window.alert('Password needs to be at least 6 characters.');
+    }
+    window.alert('That is Invalid');
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 }
 
 //------------------------------------------------------
@@ -50,49 +50,49 @@ function createUser() {
 //------------------------------------------------------ 
 function login() {
 
-    let theEmail = document.getElementById('username');
-    let pass = document.getElementById('password');
+  let theEmail = document.getElementById('username');
+  let pass = document.getElementById('password');
 
-    let email = theEmail.value;
-    let password = pass.value;
+  let email = theEmail.value;
+  let password = pass.value;
 
-    auth.signInWithEmailAndPassword(email, password).then(function () {
-        let user = firebase.auth().currentUser;
-        if (user == null) {
-            // User not signed in.
-            console.log("not logged in");
-        } else {
-            // user is signed in, send to game page.
-            location.replace('studyGuide.html');
-        }
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        window.alert('Not a valid log in.');
-        var errorMessage = error.message;
-        // ...
-    });
-    // Store the users information into the database.
+  auth.signInWithEmailAndPassword(email, password).then(function () {
+    let user = firebase.auth().currentUser;
+    if (user == null) {
+      // User not signed in.
+      console.log("not logged in");
+    } else {
+      // user is signed in, send to game page.
+      location.replace('leaderboard.html');
+    }
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    window.alert('Not a valid log in.');
+    var errorMessage = error.message;
+    // ...
+  });
 }
+
 
 //------------------------------------------------------
 // Logout
 //------------------------------------------------------ 
 function logout() {
-    firebase.auth().signOut().then(function () {
-        window.location.replace('index.html');
-    }).catch(displayError);
+  firebase.auth().signOut().then(function () {
+    window.location.replace('index.html');
+  }).catch(displayError);
 }
 
 //------------------------------------------------------
 // Send new User to database
 //------------------------------------------------------ 
 
-let user = firebase.auth().currentUser;
+let user = auth.currentUser;
 let email, curentGame;
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
     // User is signed in.
     console.log('logged in');
 
@@ -102,65 +102,62 @@ firebase.auth().onAuthStateChanged(function(user) {
 
     // Add the user info to the database.
     db.collection('Users').doc(uid).set({
-        // CurrGameId: currentGame,
-        email: userEmail,
-        UID: uid
-    }).then(function() {
-        console.log('Doc successfully written!');
-    }).catch(function(error) {
-        console.error('Error writing document: ', error);
+      // CurrGameId: currentGame,
+      email: userEmail,
+      UID: uid
+    }).then(function () {
+      console.log('Doc successfully written!');
+    }).catch(function (error) {
+      console.error('Error writing document: ', error);
     })
 
-    } else {
-      // No user is signed in.
-      console.log('not logged in');
-    }
-  });
+  } else {
+    // No user is signed in.
+    console.log('not logged in');
+  }
+});
 
 //------------------------------------------------------
 // Send Password Reset email
 //------------------------------------------------------ 
 
-// Reference firebase authentication
-let auth = firebase.auth();
+// // Get the users email address.
+// let emailAddress = "user@example.com";
 
-// Get the users email address.
-let emailAddress = "user@example.com";
+// // Send the password reset email.
+// auth.sendPasswordResetEmail(emailAddress).then(function () {
+//   // Email sent.
+// }).catch(function (error) {
+//   // An error happened.
+// });
 
-// Send the password reset email.
-auth.sendPasswordResetEmail(emailAddress).then(function() {
-  // Email sent.
-}).catch(function(error) {
-  // An error happened.
-});
+// //------------------------------------------------------
+// // Update password
+// //------------------------------------------------------ 
 
-//------------------------------------------------------
-// Update password
-//------------------------------------------------------ 
+// // Grab the currently logged in user.
+// let user = firebase.auth().currentUser;
 
-// Grab the currently logged in user.
-let user = firebase.auth().currentUser;
+// // Get the new password.
+// let newPassword = getASecureRandomPassword();
 
-// Get the new password.
-let newPassword = getASecureRandomPassword();
+// // Update the pasword in firestore.
+// user.updatePassword(newPassword).then(function () {
+//   // Update successful.
+// }).catch(function (error) {
+//   // An error happened.
+// });
 
-// Update the pasword in firestore.
-user.updatePassword(newPassword).then(function() {
-  // Update successful.
-}).catch(function(error) {
-  // An error happened.
-});
+// //------------------------------------------------------
+// // Get User's profile information
+// //------------------------------------------------------ 
 
-//------------------------------------------------------
-// Get User's profile information
-//------------------------------------------------------ 
+// var user = auth.currentUser;
+// var name, email, photoUrl, uid, emailVerified;
 
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified;
-
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-  emailVerified = user.emailVerified;
-  uid = user.uid;
-}
+// if (user != null) {
+//   name = user.displayName;
+//   email = user.email;
+//   emailVerified = user.emailVerified;
+//   uid = user.uid;
+// }
